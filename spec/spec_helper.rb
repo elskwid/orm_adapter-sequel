cac5dc8c7bfb525e2549eae04c4c49ddb23b8b3a
El Skwid (Don Morrison) <elskwid@gmail.com>
2011-01-14 00:59:48 -0800

    Remove comment about orm_adapter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,5 @@ $:.unshift(File.dirname(__FILE__) + '/../lib')
 
 require 'rspec'
 require 'sequel'
-require 'orm_adapter' # should be installed by our rake tasks
+require 'orm_adapter'
 require 'orm_adapter-sequel'
